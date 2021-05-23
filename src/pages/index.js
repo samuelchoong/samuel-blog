15ad506cd24fe9ff243543c9545c4917f24a4a6a
Samuel <samuelchoong1@gmail.com>
2021-05-23 15:24:09 +0800

    remove obsolete files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,42 @@
 import React from "react"
+import { graphql,Link } from "gatsby"
 import Layout from "../components/Layout"
 
-export default function Index() {
-  return (
-    <>
-      <Layout>
-        <h1>Hello World</h1>
-      </Layout>
-    </>
-  )
+export default function IndexPage({data}) {
+    const {totalCount,nodes} = data.allMarkdownRemark
+    return (
+        <>
+            <Layout>
+                <h4>{totalCount} Posts</h4>
+                {nodes.map(({id,fields, frontmatter,excerpt}) =>
+                    <div key={id}>
+                        <h3>
+                            {frontmatter.title}
+                            <span> - {frontmatter.date}</span>
+                        </h3>
+                        <p>{excerpt}</p>
+                        <Link to={`/blogs/${frontmatter.slug}`}>Read More</Link>
+                    </div>    
+                )}
+            </Layout>
+        </>
+    )
 }
+
+export const query = graphql`
+    query MyQuery{
+        allMarkdownRemark {
+            totalCount
+            nodes {
+                id
+                frontmatter {
+                    title
+                    date(formatString: "DD MMMM, YYYY")
+                    subtitle,
+                    slug
+                }
+                excerpt
+            }
+        }
+    }
+`
